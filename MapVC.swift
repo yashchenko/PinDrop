1ccaf8c8f17dbbcffde2d096b442b2249485b843
Ivan Yashchenko <ivan.profitx@gmail.com>
2026-09-16 03:17:34 +0300

    first commit

---
+++ MapVC.swift
@@ -0,0 +1,19 @@
+//
+//  MapVC.swift
+//  PinDrop
+//
+//  Created by Ivan on 16.09.2026.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+    
+    
+}
